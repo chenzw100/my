da602e7ede880ec07f80becf6517842de96a6d64
czw <chenzhiwei@diligrp.com>
2019-02-01 10:59:14 +0800

    init my demo

--- src/main/java/com/example/my/MyApplication.java
+++ src/main/java/com/example/my/MyApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class MyApplication extends SpringBootServletInitializer {
@@ -14,6 +16,10 @@ public class MyApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyApplication.class, args);
+	}
+	@Bean
+	public RestTemplate restTemplate(){
+		return new RestTemplate();
 	}
 
 }
