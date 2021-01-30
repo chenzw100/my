9c661e219cda25c20979bc65f52643e3aa5479eb
czw <chenzhiwei@diligrp.com>
2021-01-30 16:02:19 +0800

    666

--- src/main/java/com/example/demo/domain/table/StockTemperature.java
+++ src/main/java/com/example/demo/domain/table/StockTemperature.java
@@ -285,9 +285,9 @@ public class StockTemperature {
         sb.append(dateStr);
         sb.append("[上证:").append(getTradeVal()).append("亿、创:").append(getTradeCYBVal()).append("亿][强:").append(getSuperUpCount()).append(",大阳:").append(getSuperCount())
         .append(",正:").append(getContinueCount()).append("] [负:").append(getStrongDowns()).append(", 跌:").append(getLimitDown())
+         .append("][涨:").append(getLimitUp()).append(", 炸:").append(getOpen())
                 .append("][C强:").append(getSuperUpCountCYB()).append(",C大阳:").append(getSuperCountCYB())
                 .append(",C正:").append(getContinueCountCYB()).append("] [C负:").append(getStrongDownsCYB())
-         .append("][涨:").append(getLimitUp()).append(", 炸:").append(getOpen())
         .append("] [昨:").append(MyUtils.getYuanByCent(getYesterdayShow()))
         .append("] [连:").append(getContinueVal())
         .append("] [炸:").append(MyUtils.getYuanByCent(getBrokenRatio()))
