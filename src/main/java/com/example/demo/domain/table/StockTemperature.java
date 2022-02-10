7e2788ba49c2d38a72b13fc3696c6f077ae803bc
chenzhiwei <chenzhiwei@diligrp.com>
2022-02-10 12:08:44 +0800

    new 9992

--- src/main/java/com/example/demo/domain/table/StockTemperature.java
+++ src/main/java/com/example/demo/domain/table/StockTemperature.java
@@ -289,14 +289,11 @@ public class StockTemperature {
 
         sb.append(dateStr);
         sb.append("] 温度:").append(getTemperature())
-        .append("[量:").append(getTradeVal()).append("+").append(getTradeCYBVal()).append("亿")
-        .append("] 【核:").append(getStrongDowns()).append(", 跌:").append(getLimitDown()).append("] [上涨:").append(getRaise()).append(", 下跌:").append(getDown()).append("】")
-         .append("] [强:").append(getSuperUpCount()).append(",大阳:").append(getSuperCount()).append(",正:").append(getContinueCount())
-         .append("[涨:").append(getLimitUp()).append(", 炸:").append(getOpen())
-        .append("] [昨:").append(MyUtils.getYuanByCent(getYesterdayShow()))
-        .append("] [连:").append(getContinueVal())
-        .append("] [炸:").append(MyUtils.getYuanByCent(getBrokenRatio()))
-         .append("][C强:").append(getSuperUpCountCYB()).append(",C大阳:").append(getSuperCountCYB()).append(",C正:").append(getContinueCountCYB()).append("] [C负:").append(getStrongDownsCYB())
+        .append("[量:").append(getTradeVal()).append("+").append(getTradeCYBVal()).append("亿]")
+        .append("【核:").append(getStrongDowns()).append(",跌:").append(getLimitDown()).append("] [上涨:").append(getRaise()).append(",下跌:").append(getDown()).append("】")
+         .append("[强:").append(getSuperUpCount()).append(",大阳:").append(getSuperCount()).append(",正:").append(getContinueCount()).append(",涨:").append(getLimitUp()).append(",炸:").append(getOpen())
+        .append("][昨:").append(MyUtils.getYuanByCent(getYesterdayShow())).append(",[连:").append(getContinueVal()).append(",[炸:").append(MyUtils.getYuanByCent(getBrokenRatio()))
+         .append("][C强:").append(getSuperUpCountCYB()).append(",C大阳:").append(getSuperCountCYB()).append(",C正:").append(getContinueCountCYB()).append("[C负:").append(getStrongDownsCYB())
                 .append("]<br>");
 
         return sb.toString();
