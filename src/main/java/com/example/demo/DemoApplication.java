81d789030a2afa02d17cc16fa03bc0aaa3ba208c
chenzw <chenzw100@163.com>
2023-02-11 08:32:17 +0800

    d test

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -10,6 +10,7 @@ import org.springframework.web.client.RestTemplate;
 @EnableScheduling
 public class DemoApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
